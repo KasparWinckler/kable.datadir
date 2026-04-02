57a7eaabb3f5888e4caec28fe7eadc171de54b2c
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2026-04-02 05:52:39 +0200

    Initial commit

---
+++ update.sh
@@ -0,0 +1,6 @@
+python3 create_repository.py \
+  https://github.com/KasparWinckler/kable.repository.git#0.0.1 \
+  https://github.com/KasparWinckler/kable.dependencies.git#0.0.2 \
+  https://github.com/KasparWinckler/kable.framework.git#0.0.2 \
+  https://github.com/KasparWinckler/kable.plugin.france_tv.git#0.0.2 \
+  https://github.com/KasparWinckler/kable.plugin.radiofrance.git#0.0.2 \
