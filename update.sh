c5abd2c9fcf50b5e8a6e0f1ef8086347aa37a0f5
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2026-05-22 08:42:22 +0200

    Update addons

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ python3 create_repository.py \
   https://github.com/KasparWinckler/kable.dependencies.git#0.0.2 \
   https://github.com/KasparWinckler/kable.framework.git#0.0.2 \
   https://github.com/KasparWinckler/kable.plugin.france_tv.git#0.0.2 \
-  https://github.com/KasparWinckler/kable.plugin.radiofrance.git#0.0.2 \
+  https://github.com/KasparWinckler/kable.plugin.radiofrance.git#0.0.3 \
